885a313bb45e6838d28978bb4c6df388b31d3735
paderborner2019 <paderborner2019@gmail.com>
2020-06-10 13:31:16 +0300

    without dist

--- Backend/dist/features/shared/enums/userFilterType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var UserFilterType;
-(function (UserFilterType) {
-    UserFilterType[UserFilterType["All"] = 0] = "All";
-    UserFilterType[UserFilterType["Active"] = 1] = "Active";
-    UserFilterType[UserFilterType["Blocked"] = 2] = "Blocked";
-})(UserFilterType = exports.UserFilterType || (exports.UserFilterType = {}));
-//# sourceMappingURL=userFilterType.js.map
--- Backend/dist/features/shared/enums/userStatus.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var UserStatus;
-(function (UserStatus) {
-    UserStatus[UserStatus["unblocked"] = 0] = "unblocked";
-    UserStatus[UserStatus["block"] = 1] = "block";
-})(UserStatus = exports.UserStatus || (exports.UserStatus = {}));
-//# sourceMappingURL=userStatus.js.map
